f2399facae6ba1ec255d8e2aa0e8556ad5b4ea43
Richardaeld <richardaeld@gmail.com>
2020-10-29 06:57:28 +0000

    Made guild, church of steve image rotatable on index.html

---
+++ static/js/playground.js
@@ -0,0 +1,6 @@
+document.getElementById("steve").onclick = function() {
+    document.getElementById("steve1").classList.add("flip")
+    console.log(document.getElementById("steve1").classList)
+
+}
+
