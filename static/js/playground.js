d848fac02b07e47ab6aa4e42dbb0fcc3ca164a55
Richardaeld <richardaeld@gmail.com>
2020-11-02 20:28:48 +0000

    Improved modal for 'temple of steve' on index.html

--- static/js/playground.js
+++ static/js/playground.js
@@ -1,4 +1,9 @@
 document.getElementById("ste").onclick = function () {
     document.getElementById("parch").classList.add("parchdis")
-    console.log("murder hobo")
+    document.getElementById("modal-cover").classList.add("parchdis")
+}
+
+document.getElementById("modal-cover").onclick = function() {
+    document.getElementById("parch").classList.remove("parchdis")
+    document.getElementById("modal-cover").classList.remove("parchdis")
 }
