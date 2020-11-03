297c0e553e84625b413185c901ef077698d6504b
Richardaeld <richardaeld@gmail.com>
2020-11-03 00:48:48 +0000

    unpolished additions to 'temple ...'  modal transitions(pre appropiate JS code) on index.html

--- static/js/playground.js
+++ static/js/playground.js
@@ -1,9 +1,16 @@
 document.getElementById("ste").onclick = function () {
     document.getElementById("parch").classList.add("parchdis")
+    document.getElementById("parch").classList.add("box")
+    document.getElementById("parch").classList.add("box2")
     document.getElementById("modal-cover").classList.add("parchdis")
+    document.getElementById("modal-cover").classList.add("box1")
+
+
 }
 
 document.getElementById("modal-cover").onclick = function() {
     document.getElementById("parch").classList.remove("parchdis")
+    document.getElementById("parch").classList.remove("box")
     document.getElementById("modal-cover").classList.remove("parchdis")
+    document.getElementById("modal-cover").classList.remove("box")
 }
