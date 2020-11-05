28730e6d644a596315def9c86fe7ec669668d485
Richardaeld <richardaeld@gmail.com>
2020-11-05 04:05:49 +0000

    removed unneeded images, uploaded new compressed images, renamed guild files, and added 'page title headers' to base.html"

--- run.py
+++ run.py
@@ -7,24 +7,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", title_header="Role Players Guild")
 
 
 @app.route("/temple")
 def temple():
-    return render_template("temple.html")
+    return render_template("temple.html", title_header="Temple of Steve")
 
 
 @app.route("/hallow")
 def hallow():
-    return render_template("hallow.html")
-
+    return render_template("hallow.html", title_header="Hallow Herd")
 
 
 @app.route("/tomb")
 def tomb():
-    return render_template("tomb.html")
-
+    return render_template("tomb.html", title_header="Tomb of Annihilation")
 
 
 if __name__ == "__main__":
