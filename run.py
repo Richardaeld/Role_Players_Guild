06777eb6112c745c8d4221571694e8c10d7e302b
Richardaeld <richardaeld@gmail.com>
2020-11-05 05:01:13 +0000

    updated individual headers for all pages from base.html and imporved text of 'hallow...' guild card on index.html

--- run.py
+++ run.py
@@ -7,22 +7,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", title_header="Role Players Guild")
+    return render_template("index.html", title_header="Role Players Guild", header_img="index-header-img")
 
 
 @app.route("/temple")
 def temple():
-    return render_template("temple.html", title_header="Temple of Steve")
+    return render_template("temple.html", title_header="Temple of Steve", header_img="steve-header-img")
 
 
 @app.route("/hallow")
 def hallow():
-    return render_template("hallow.html", title_header="Hallow Herd")
+    return render_template("hallow.html", title_header="In the Shadow of the Hallow Herd", header_img="hallow-header-img")
 
 
 @app.route("/tomb")
 def tomb():
-    return render_template("tomb.html", title_header="Tomb of Annihilation")
+    return render_template("tomb.html", title_header="Tomb of Annihilation", header_img="tomb-header-img")
 
 
 if __name__ == "__main__":
