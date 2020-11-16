88c6b74022d76c721cab034b1cf964a5233cef3a
Richardaeld <richardaeld@gmail.com>
2020-11-16 17:54:13 +0000

    Added ability for flask to view DB and updated requirements.txt

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 import os
-from flask import Flask, render_template
+from flask import (
+    Flask, flash, render_template, 
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
@@ -7,8 +11,22 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+mongo = PyMongo(app)
+
+
 @app.route("/")
-def index():
+@app.route("/get_tasks")
+def get_tasks():
+    tasks = mongo.db.templeOfSteve.find()
+    return render_template("tasks.html", templeOfSteve=tasks)
+
+@app.route("/home")
+def home():
     return render_template("index.html", title_header="Role Players Guild", header_img="index-header-img")
 
 
