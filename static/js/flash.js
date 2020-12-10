3d6d8e661a59ac8f70c6dde70121da14cc8306b1
Richardaeld <richardaeld@gmail.com>
2020-12-10 19:01:23 +0000

    removed a bug that added blank <li> into forms and fixed a span hover bug on 'temple.html' front page

--- static/js/flash.js
+++ static/js/flash.js
@@ -1,3 +1,5 @@
+//-------------------post data base CRUD function JS -----------------------------
+
 // removes the flash message after 3 seconds
 var flashCheck = document.querySelector(".flash");
 if (flashCheck != null){
@@ -5,4 +7,14 @@ if (flashCheck != null){
         console.log("removing")
         document.getElementsByClassName("flash")[0].style.display = "none";
     },3000);
+}
+
+
+// set width of temple spans
+var getitemWidth = document.getElementsByClassName("temple-cats")[0].offsetWidth;
+var getSpanCount = document.querySelectorAll(".temple-span");
+getSpanCount.forEach(setSpanWidth);
+
+function setSpanWidth(item, index) {
+    document.getElementsByClassName("temple-span")[index].style.cssText = "width:" + getitemWidth + "px";
 }
