51bd7844b3e8c43dbc4910df57f167eb4835bd47
Richardaeld <richardaeld@gmail.com>
2020-12-17 18:36:49 +0000

    corrected responsiveness bug on 'signin.html'

--- run.py
+++ run.py
@@ -70,7 +70,7 @@ def register():
     return render_template(
         "register.html",
         header_img_class="col-12 profile-header",
-        header_img="log-img",
+        header_img="sign-reg-background",
         header_title_class="header-title",
         title_header="",
         title_header_p="")
@@ -103,7 +103,7 @@ def signIn():
     return render_template(
         "signIn.html",
         header_img_class="col-12 profile-header",
-        header_img="rotate-background",
+        header_img="sign-reg-background",
         header_title_class="header-title",
         title_header="",
         title_header_p="")
