c31cdc585fb97cb43f86f3ea87af43f1256cb3a3
Richardaeld <richardaeld@gmail.com>
2020-12-18 03:20:24 +0000

    added JS to support 'room_form.html'

--- static/js/room.js
+++ static/js/room.js
@@ -7,8 +7,8 @@ findCommitList.forEach(makeClick)
 function makeClick(item, index){
     document.getElementsByClassName("commit-box")[index].addEventListener('click', function () {
         console.log("click" + index);
-
-
+        var commitText = document.getElementsByClassName("commit-box")[index].textContent
+        document.getElementById("form-expand-to").textContent = commitText;
     })
 }
 
